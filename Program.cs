065d015e66b2b31a7717ea57191b0cdbc1a06045
brad-morrison <45197952+brad-morrison@users.noreply.github.com>
2019-06-17 14:04:22 +0100

    Added Searches to menu functionality

--- Program.cs
+++ Program.cs
@@ -12,27 +12,86 @@ namespace BST
         }
 
         public static Node root = null;
+        public static bool treeExists = false;
 
         static void Main(string[] args)
         {
-            // input data to be sorted into tree
-            int[] data = new int[] {51, 34, 13, 99, 9, 15, 33, 20, 56, 78, 42, 96, 122, 533, 566, 623, 784, 998};
-            int[] dataRand = RandArray(78);
-            
-            // create nodes for each element and add to tree
-            for (int i = 0; i < data.Length; i++)
-            {
-                Console.WriteLine("Node " + i + " created");
-                TreeInsertion(data[i]);
-            }
+            loadMenu();
 
-            Console.WriteLine("TREE CREATED");
-            Console.WriteLine("Min value of tree is " + SearchMin().value);
-            Console.WriteLine("Max value of tree is " + SearchMax().value);
-            Node search = Search(33);
-            Console.WriteLine("Node: " + search + " Node value: " + search.value + "    Child Left: " + search.leftChild + "    Child Right: " + search.rightChild);
+            
+            //Console.WriteLine("Min value of tree is " + SearchMin().value);
+            //Console.WriteLine("Max value of tree is " + SearchMax().value);
+            //Node search = Search(33);
+            //Console.WriteLine("Node: " + search + " Node value: " + search.value + "    Child Left: " + search.leftChild + "    Child Right: " + search.rightChild);
         }
 
+        public static void loadMenu()
+        {
+            Console.Clear();
+            string treeExists_string;
+            if (treeExists) {   treeExists_string = "TREE EXISTS";  }  else {  treeExists_string = "EMPTY"; }
+
+            Console.WriteLine("\n Tree [" + treeExists_string + "]");
+            Console.WriteLine("\n\n Enter option: \n 1 - Create Tree \n 2 - Search Min Node \n 3 - Search Max Node \n 4 - Search Node");
+            UserOption();
+        }
+
+        public static void UserOption()
+        {
+            string option;
+            option = Console.ReadLine();
+            int optionInt = Convert.ToInt32(option);
+
+            switch(optionInt) 
+            {
+                case 1: Console.Clear();
+                        Console.WriteLine("How big?: ");
+                        string a;
+                        a = Console.ReadLine();
+                        CreateTree(Convert.ToInt32(a));
+                        break;
+
+                case 2: Console.Clear();
+                        Console.WriteLine("The smallest value in the tree is " + SearchMin().value);
+                        Console.WriteLine("\n\nPress any key to continue...");
+                        Console.ReadKey();
+                        loadMenu();
+                        break;
+
+                case 3: Console.Clear();
+                        Console.WriteLine("The largest value in the tree is " + SearchMax().value);
+                        Console.WriteLine("\n\nPress any key to continue...");
+                        Console.ReadKey();
+                        loadMenu();
+                        break;
+
+                case 4: Console.Clear();
+                        Console.WriteLine("Enter value to search: ");
+                        string b;
+                        b = Console.ReadLine();
+                        Node search = Search(Convert.ToInt32(b));
+                        if (search == null)
+                        {
+                            Console.WriteLine("Node not found in tree");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Node Found: [" + search + "] -- Value: [" + search.value + "] LeftChild: [" + search.leftChild + "] RightChild: [" + search.rightChild + "]");
+                        }
+                        Console.WriteLine("\n\nPress any key to continue...");
+                        Console.ReadKey();
+                        loadMenu();
+                        break;
+
+                default:    Console.Clear();
+                            Console.WriteLine("Please enter an option between 1 and 5 \n\nPress and key to continue...");
+                            Console.ReadKey();
+                            loadMenu();
+                            break;
+
+            }
+        }
+        
         public static int[] RandArray(int max)
         {
             int[] randArray = new int[max];
@@ -46,6 +105,22 @@ namespace BST
             }
             
             return randArray;
+        }
+
+        public static void CreateTree(int size)
+        {
+            int[] dataRand = RandArray(size);
+
+            // create nodes for each element and add to tree
+            for (int i = 0; i < dataRand.Length; i++)
+            {
+                Console.WriteLine("Node " + i + " created");
+                TreeInsertion(dataRand[i]);
+            }
+
+            Console.WriteLine("TREE CREATED");
+            treeExists = true;
+            loadMenu();
         }
         
         public static void TreeInsertion(int value) // Create Tree/Insert Node
@@ -131,14 +206,12 @@ namespace BST
                 // if fails
                 if (curNode == null)
                 {
-                    Console.WriteLine("Node not found");
                     return null;
                 }
 
                 //if val = node value
                 if (value == curNode.value)
                 {
-                    Console.WriteLine("Node Found");
                     return curNode;
                 }
             }
